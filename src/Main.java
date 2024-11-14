12203dd5c9464b8bc5bd235f3913768de6b57a17
DeRick Martin II <cancermoon365@gmal.com>
2024-11-14 11:48:04 -0600

    Robot Lab

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
+class main {
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot(); // Create a new Robot object
+        myRobot.move(); // Make the robot move
+        myRobot.checkBatteryLevel(); // Check battery level
+        myRobot.stop(); // Stop the robot
+        myRobot.chargebattery(); // Charge the robot's battery
+    }
+}
+git init
+
