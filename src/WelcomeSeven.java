e62c6375f36a46af59116da779c7841026e2f307
DeRick Martin II <cancermoon365@gmal.com>
2024-11-19 11:20:16 -0600

    303.2.1 - Core Java: Operators

---
+++ src/WelcomeSeven.java
@@ -0,0 +1,11 @@
+public class WelcomeSeven {
+    public static class ForLoop {
+        public static void main(String arags[]) {
+            // For loop to iterate until x < 10
+            for(int x=0; x< 7; x++) {
+                System.out.println("Welcome to Java: " + x);
+            }
+        }
+    }
+
+}
